be506e700a1f5ab4add292d1072a5270ab9dcecb
Jason Etcovitch <jasonetco@github.com>
2018-12-14 21:57:41 -0500

    Write some basic tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,52 +1,47 @@
-// You can import your modules
-// import index from '../src/index'
-
 import nock from 'nock'
-// Requiring our app implementation
-import myProbotApp from '../src'
 import { Probot } from 'probot'
-// Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+import goodFirstPin from '../src'
+
+const payload = require('./fixtures/issues.labeled.json')
 
 nock.disableNetConnect()
 
-describe('My Probot app', () => {
-  let probot: any
+describe('good-first-pin', () => {
+  let probot: Probot
+  let event: any
 
   beforeEach(() => {
     probot = new Probot({ id: 123, cert: 'test' })
-    // Load our app into probot
-    const app = probot.load(myProbotApp)
 
-    // just return a test token
+    // Load our app into probot
+    const app = probot.load(goodFirstPin)
     app.app = () => 'test'
+
+    event = { name: 'issues', payload }
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
-    // Test that we correctly return a test token
+  it('pins a comment when an issue is labeled with a matching label', async () => {
     nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
-
-    // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
-        return true
+      .post('/graphql').reply(200, { data: { repository: { pinnedIssues: { nodes: [] } } } })
+      .post('/graphql').reply(200, {
+        data: { pinIssue: { issue: { title: payload.issue.title } } }
       })
-      .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive(event)
+    expect(nock.isDone()).toBe(true)
+  })
+
+  it('does nothing if the label added does not match', async () => {
+    nock('https://api.github.com').post('/graphql').reply(200)
+
+    // Rename to the label to something that doesn't match
+    event.payload.label.name = 'nope'
+
+    // Receive a webhook event
+    await probot.receive(event)
+
+    // If this is false, it never made a GraphQL request
+    expect(nock.isDone()).toBe(false)
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
